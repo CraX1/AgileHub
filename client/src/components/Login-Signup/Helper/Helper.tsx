822b0a25d3643930244fcfa54c4c55fc02cac736
CraX1 <rrs8503988@gmail.com>
2024-04-04 15:04:58 +0530

    AH_new: add

--- client/src/components/Login-Signup/Helper/Helper.tsx
+++ client/src/components/Login-Signup/Helper/Helper.tsx
@@ -7,3 +7,29 @@ export const socialMediaOptionWrapper = (
     {platform}
   </p>
 );
+
+export const emailRegexValidation = (emailInput: string) => {
+  console.log("computingEMAIL", emailInput);
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(emailInput);
+};
+
+export const passwordRegexValidation = (passwordInput: string) => {
+  console.log("computingpass", passwordInput);
+
+  const passwordRegex = //NOTE:1
+    // /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  return passwordRegex.test(passwordInput);
+};
+
+export const debounceHandler = (callback: Function, delay: number) => {
+  let timeoutId: NodeJS.Timeout;
+  return (...args: any[]) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+};
--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,31 +1,116 @@
 import { Users } from "../models/authModel";
+import { createSecretToken } from "../utils/SecretToken";
+import bcrypt from "bcrypt";
+import { isValidEmail, isValidPassword } from "../utils/inputValidation";
 
 export const createUser = async (req: any, res: any) => {
   try {
+    const { email, password, username } = req.body;
+
+    if (!email || !password || !username) {
+      return res.json({ error: "All fields are required" });
+    }
+    if (!isValidEmail(email) && !isValidPassword(password)) {
+      return res
+        .status(400)
+        .json({ error: "Invalid email and password format. Please re-enter" });
+    }
+    if (!isValidEmail(email)) {
+      return res
+        .status(400)
+        .json({ error: "Invalid email format. Please re-enter" });
+    }
+    if (!isValidPassword(password)) {
+      return res
+        .status(400)
+        .json({ error: "Invalid password format. Please re-enter" });
+    }
+
+    const existingUser = await Users.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ error: "User already exists" });
+    }
+
     const newUser = await Users.create(req.body);
+    const token = createSecretToken(newUser._id);
+
+    res.cookie("token", token, {
+      //NOTE 1
+      httpOnly: true, //Note 2
+      withCredentials: true,
+      // secure:true,//if using https
+      sameSite: "strict",
+      maxAge: 259200,
+    });
 
     console.log("req", req.body);
-    res.status(200).json({
-      status: "success",
+    res.status(201).json({
+      success: true,
+      message: `Created user with email, ${email}`,
       data: {
-        tour: `Created user with credentials, ${newUser}`,
+        email,
+        // username
       },
     });
   } catch (err) {
-    console.log("sasas", err);
+    res.status(500).json({
+      success: false,
+      error: `Reason: ${err}`,
+    });
   }
 };
 
-export const loginUser = (req: any, res: any) => {
+export const loginUser = async (req: any, res: any) => {
   try {
-    console.log("req", req.body);
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await Users.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User does not exist with this email" });
+    }
+
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+      return res
+        .status(401)
+        .json({ message: "Incorrect password, please re-enter" });
+    }
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      httpOnly: true, //Note 2
+      withCredentials: true,
+      // secure:true,//if using https
+      sameSite: "strict",
+      maxAge: 259200,
+    });
     res.status(200).json({
       status: "success",
+      message: `Logged in user with email, ${email}`,
       data: {
-        tour: "<>Updated tour here if required</>",
+        email,
+        username: user.username,
       },
     });
   } catch (err) {
-    console.log("sasas", err);
+    res.status(500).json({
+      success: false,
+      error: `Reason: ${err}`,
+    });
   }
 };
+
+//NOTE 1:
+// Here we set the JWT token as an HTTP-Only cookie in the response from the server
+// The client doesn't need to store or handle the token explicitly. The browser will automatically include the cookie in subsequent requests to the same domain.
+
+//NOTE 2:
+// When the httpOnly flag is set on a cookie, it instructs the web browser that the cookie should only be accessible by the server and not by client-side scripts
+// running in the browser. This means that even if a cross-site scripting (XSS) vulnerability exists on the website, and a malicious script is injected and
+// executed in the user's browser, the script will not be able to access or manipulate the cookie marked as httpOnly.
+// The withCredentials option is set to true to allow the cookie to be sent with cross-origin requests
